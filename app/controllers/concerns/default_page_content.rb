a57a83393553d4850d5520071a9263292f7d590a
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-11 16:21:10 +0000

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,16 @@
+module DefaultPageContent
+     extend ActiveSupport::Concern
+    
+    included do
+         before_action :set_page_defaults
+    end
+    
+   def set_page_defaults
+    @page_title = "DevcampPortfolio | My portfolio website"
+    @seo_keywords = "Nicholas Brannum Portfolio"
+   end
+end
+
+
+  
+ 
