1cadb541d4b55822dc0ea234aa0705b45f4cf856
Skyridge Devcamp <skyridge@devcamp.com>
2017-08-01 14:11:54 +0000

    Part of twitter gem install but decided to not finish

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
